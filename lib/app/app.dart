5d29d7d16c9560b62e04b06b081cea4345c62489
Rakibul-E404 <smrat246@gmail.com>
2026-08-17 10:39:45 +0600

    full screen done

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '../core/theme/app_theme.dart';
@@ -6,8 +7,44 @@ import '../data/repositories/local_shop_repository.dart';
 import '../presentation/screens/app_gate.dart';
 import '../presentation/state/shop_store.dart';
 
-class ShopManagerApp extends StatelessWidget {
+class ShopManagerApp extends StatefulWidget {
   const ShopManagerApp({super.key});
+
+  @override
+  State<ShopManagerApp> createState() => _ShopManagerAppState();
+}
+
+class _ShopManagerAppState extends State<ShopManagerApp>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _enableFullScreen();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _enableFullScreen();
+    }
+  }
+
+  void _enableFullScreen() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
   _initDesktopSqlite();
   runApp(const ShopManagerApp());
 }
